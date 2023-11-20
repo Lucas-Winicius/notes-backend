acfd5308db9d3c871d8b25b0d11e480e47a02854
Lucas-Winicius <lucasclucas7@gmail.com>
2023-11-20 01:33:46 -0300

    cors

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import routes from "./src/routes/router";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
