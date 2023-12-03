6bdf20d36ab43a2b3853a39d991a9f404b0d50ed
Lucas-Winicius <lucasclucas7@gmail.com>
2023-12-03 13:44:11 -0300

    cors

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: process.env.ALLOWED_URL,
+  origin: "*",
   optionsSuccessStatus: 200
 }));
 app.use(routes);
